93e25a481ed106e103053880f3a44d1685d31028
James Curtis <jameswcurtis94@outlook.com>
2024-07-31 13:16:26 +0000

    james

--- script.js
+++ script.js
@@ -11,17 +11,3 @@
                 }
             });
          });
-
-         document.getElementById('newsletterForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-
-            const name = document.getElementById('name').value;
-            const email = document.getElementById('email').value;
-
-            if (name && email) {
-                document.getElementById('newsletterForm').style.display = 'none';
-                document.getElementById('thankYouMessage').style.display = 'block';
-            } else {
-                alert('Please fill in all fields.');
-            }
-        });
